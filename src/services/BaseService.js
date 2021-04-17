ef8c483a142baabfbd6cf604697e0fda0c5cd93e
Ángela Herrador <angelaherrador1991@gmail.com>
2021-04-17 17:14:03 +0200

    Add login

---
+++ src/services/BaseService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { getAccessToken, logout } from "../store/AccessTokenStore";
+
+export const create = (opts = {}) => {
+  const http = axios.create({
+    baseURL: "http://localhost:27017/lab-profile-app",
+    ...opts,
+  });
+
+  http.interceptors.request.use((request) => {
+    if (opts.useAccessToken !== false) {
+      request.headers.common.Authorization = `Bearer ${getAccessToken()}`;
+    } else {
+      delete request.headers.common.Authorization;
+    }
+
+    return request;
+  });
+
+  http.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+      if (
+        opts.reloadOnUnathorized &&
+        error.response &&
+        [401, 403].includes(error.response.status)
+      ) {
+        logout();
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return http;
+};
